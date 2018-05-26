8aeb84d17c8a38f4f96a0b65849f44e3d86757f0
MisaelMdz <misalon.mm@gmail.com>
2018-05-26 18:54:38 -0500

    Splash Inicio

--- src/test/java/com/gymforce/Inicio.java
+++ src/test/java/com/gymforce/Inicio.java
@@ -1,8 +1,25 @@
 package com.gymforce;
 
-public class Inicio {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+public class Inicio extends javafx.application.Application{
+ 
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root =FXMLLoader.load(getClass().getResource("/com/gymforce/vista/FXMLsplash.fxml"));
+		Scene escena = new Scene(root);
+		primaryStage.setScene(escena);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.show();
 		
 	}
 
