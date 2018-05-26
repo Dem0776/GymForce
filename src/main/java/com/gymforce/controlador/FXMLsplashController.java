9d8e71485f2c02c045b77d5fcccc3f7694940a4b
Dem0776 <guspetrearce@gmail.com>
2018-05-26 12:32:51 -0500

    actualizacion

---
+++ src/main/java/com/gymforce/controlador/FXMLsplashController.java
@@ -0,0 +1,5 @@
+package com.gymforce.controlador;
+
+public class FXMLsplashController {
+
+}
---
+++ src/test/java/com/gymforce/Inicio.java
@@ -0,0 +1,9 @@
+package com.gymforce;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
