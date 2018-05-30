63f7475dd0cc2bd5b0e9dc7c72f5d73bd8ebc212
or20saes <osanchezescobar@gmail.com>
2018-05-29 21:54:27 -0500

    Conexion splash-login

---
+++ src/main/java/com/gymforce/controlador/FXMLoginController.java
@@ -0,0 +1,16 @@
+package com.gymforce.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FXMLoginController {
+	@FXML
+	void accederMenu(ActionEvent event) {
+
+	}
+
+	@FXML
+	void salirAplicacion(ActionEvent event) {
+		System.exit(0);
+	}
+}
