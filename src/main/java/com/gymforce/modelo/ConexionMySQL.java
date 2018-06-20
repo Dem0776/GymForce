a1ecd69a15f6ed444a5a1273a60160dfc49c7324
or20saes <osanchezescobar@gmail.com>
2018-06-20 17:33:39 -0500

    ins

--- src/main/java/com/gymforce/modelo/ConexionMySQL.java
+++ src/main/java/com/gymforce/modelo/ConexionMySQL.java
@@ -6,14 +6,11 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.JOptionPane;
-
 public class ConexionMySQL {
 	private Connection connection;
-	
 	private String url = "jdbc:mysql://localhost/gymforce";
-	private String usuario = "gustavo";
-	private String contrasena = "midian776";
+	private String usuario = "root";
+	private String contrasena = "1";
 
 	public Connection getConnection() {
 		return connection;
@@ -27,7 +24,6 @@ public class ConexionMySQL {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(url, usuario, contrasena);
-			//JOptionPane.showMessageDialog(null,"Conexion Establecida");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
